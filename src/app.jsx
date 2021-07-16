70cad26a67662883057154043b6c43f90b446a91
Yunsu <kenatman0723@gmail.com>
2021-07-16 13:09:32 +0900

    fix the Router & add .env

--- src/app.jsx
+++ src/app.jsx
@@ -3,12 +3,43 @@ import { HashRouter, Route } from "react-router-dom";
 import "./app.css";
 import Contents from "./components/contents";
 import Detail from "./components/detail";
+import axios from "axios";
+import Header from "./components/header";
 
 class App extends Component {
+  state = { videos: [] };
+
+  getVideos = async () => {
+    const {
+      data: { items },
+    } = await axios.get(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${process.env.REACT_APP_API_KEY}`
+    );
+
+    this.setState({ videos: items });
+  };
+
+  componentDidMount() {
+    this.getVideos();
+  }
+
+  handleSearch = async (term) => {
+    const {
+      data: { items },
+    } = await axios.get(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${term}&key=${process.env.REACT_APP_API_KEY}`
+    );
+
+    this.setState({ videos: items });
+  };
+
   render() {
     return (
       <HashRouter>
-        <Route path="/" exact={true} component={Contents} />
+        <Header onSearch={this.handleSearch} />
+        <Route path="/" exact={true}>
+          <Contents contents={this.state.videos} />
+        </Route>
         <Route path="/video/:id" component={Detail} />
       </HashRouter>
     );
--- src/components/contents.jsx
+++ src/components/contents.jsx
@@ -2,39 +2,13 @@ import React, { Component } from "react";
 import axios from "axios";
 import Content from "./content";
 import "./contents.css";
-import Header from "./header";
+
 class Contents extends Component {
-  state = { videos: [] };
-
-  getVideos = async () => {
-    const {
-      data: { items },
-    } = await axios.get(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBDY0VSFooCB3BuL6bFSl78SqZbcdjZoAM"
-    );
-
-    this.setState({ videos: items });
-  };
-
-  componentDidMount() {
-    this.getVideos();
-  }
-
-  handleSearch = async (term) => {
-    const {
-      data: { items },
-    } = await axios.get(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${term}&key=AIzaSyBDY0VSFooCB3BuL6bFSl78SqZbcdjZoAM`
-    );
-
-    this.setState({ videos: items });
-  };
   render() {
     return (
       <>
-        <Header onSearch={this.handleSearch} />
         <ul className="contents-ul">
-          {this.state.videos.map((content) => (
+          {this.props.contents.map((content) => (
             <Content
               key={content.id.videoId ? content.id.videoId : content.id}
               content={content}
